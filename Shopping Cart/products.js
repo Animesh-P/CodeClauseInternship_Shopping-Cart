f061f5d4a69c28e13154ef45ed8e0bcb0b9e0eca
Animesh-P <101737846+Animesh-P@users.noreply.github.com>
2024-12-27 10:31:46 +0530

    Add files via upload

---
+++ Shopping Cart/products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        "id": 1,
+        "name":"Corner sofa-bed with storage Faringe light grey",
+        "price": 50999,
+        "image": "images/1.png",
+        "description": "This sofa converts quickly and easily into a spacious bed when you remove the back cushions and pull out the underframe. Sofa, chaise longue and double bed in one."
+    },
+    {
+        "id": 2,
+        "name":" 3-seat sofa dark blue",
+        "price": 19000,
+        "image": "images/2.png",
+        "description": "Durable metal springs in the seat give the sofa a springy comfort, allowing you to sit, relax and enjoy it for many years. Seat cushions filled with high resilience foam and polyester fibre wadding give comfortable support for your body, and easily regain their shape when you get up."
+    },
+    {
+        "id": 3,
+        "name":"Plastic Chairs ",
+        "price": 1000,
+        "image": "images/3.png",
+        "description": "Brand	PETAL Colour	Choco Brown Material	Plastic Product Dimensions	53D x 44W x 88H Centimeters Size	Set of 4 Back Style	Solid Back"
+    },
+    {
+        "id": 4,
+        "name":" Duvet cover and pillowcase",
+        "price": 200,
+        "image": "images/4.png",
+        "description": "The cotton/polyester blend gives you a long-lasting material that irons out easily. The cotton breathes and absorbs moisture while the polyester does not shrink, dries quickly and is wrinkle-free.The polyester in this fabric is 100% recycled."
+    },
+    {
+        "id": 5,
+        "name":" Frame black",
+        "price": 300,
+        "image": "images/5.png",
+        "description": "This frame is ideal to use with ALFTA adhesive hook. With the hook you can easily hang the frame without nails or screws and decorate your walls with pictures. Can be used hanging or standing, both horizontally and vertically, to fit in the space available.."
+    },
+    {
+        "id": 6,
+        "name":" Food container, set of 17, transparent/green",
+        "price": 200,
+        "image": "images/6.png",
+        "description": "Food container, set of 17, transparent/green"
+    },
+    {
+        "id": 7,
+        "name":" Cabinet with doors, black-blue,",
+        "price": 200,
+        "image": "images/7.png",
+        "description": "The furniture in the SKRUVBY series is designed to go well together. Combine and match them as you like or use each piece of furniture as a beautiful solitaire."
+    },
+    {
+        "id": 8,
+        "name":"Plant",
+        "price": 200,
+        "image": "images/8.png",
+        "description": " Potted plant, Golden Pothos Country of Origin India"
+    }
+
+];
+export default products;
